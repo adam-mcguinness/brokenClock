212be468bdfd30086ff448a3cd71e9c6d9f63170
adam <adam@innope.com>
2024-08-21 12:58:19 -0600

    new config

--- config.py
+++ config.py
@@ -8,15 +8,15 @@ clock_face_line_width = 2
 # Quarter Hour Markers
 add_quarter_hour_markers = True
 quarter_hour_line_color = 'black'
-quarter_hour_line_length = 2.3
-quarter_hour_line_width = 1.6
+quarter_hour_line_length = 1.5
+quarter_hour_line_width = 1.2
 quarter_hour_line_offset = 0
 
 # 5 Minute Markers
 add_5_minute_markers = True
 five_minute_color = 'black'
-five_minute_line_length = 1.3
-five_minute_line_width = 1.1
+five_minute_line_length = 1
+five_minute_line_width = 0.8
 five_minute_line_offset = 0
 
 # 1 Minute Markers
@@ -28,17 +28,17 @@ minute_line_offset = 0.95
 
 # Hands
 hand_color = 'black'
-hour_hand_length = 7.25
-hour_hand_width = 2
-minute_hand_length = 11
-minute_hand_width = 1.4
+hour_hand_length = 5
+hour_hand_width = 1.5
+minute_hand_length = 8
+minute_hand_width = 1
 
 # Layout and Size
 units = 'mm'  # options are inches, feet, cm, or mm
-grid_rows, grid_cols = 24, 30
-clock_diameter = 30
-spacing = 10
-outside_spacing = 20
+grid_rows, grid_cols = 18, 40
+clock_diameter = 20
+spacing = 8
+outside_spacing = 15
 background_color = 'white'
 randomize_clocks = True
 
